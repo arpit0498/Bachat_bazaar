79301abbaceb3c16d1d085c2db8e18d19b0569ff
arpit gupta <mycontact.arpit@gmail.com>
2023-06-07 02:34:24 +0530

    slider

--- bachat_bazaar/src/components/Slider/Slider.jsx
+++ bachat_bazaar/src/components/Slider/Slider.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const slides = [
     "https://m.media-amazon.com/images/G/31/img23/Sports/June/Yogaday/Sports_store/Yoga-day_Blue-3000x770._SX3000_QL85_.jpg",
     "https://m.media-amazon.com/images/G/31/img23/Sports/March/Sports_Store/Header/2/Under-499_Header-design_3000x770._SX3000_QL85_.jpg",
-    "https://m.media-amazon.com/images/G/31/img23/Sports/March/Sports_Store/Header/1/Final/Customers-most-loved-Header-design_3000x7701._CB590250337_.gif",
+    "https://m.media-amazon.com/images/G/31/img23/Sports/March/Sports_Store/Header/1/Final/Customers-most-loved-Header-design_3000x7701._CB590250337_.gif"
 ];
 
 export function Slider() {
@@ -21,7 +21,7 @@ export function Slider() {
 
     return (
         <div className="slider-container">
-            <Link to="/store">
+            <Link to="/product">
                 <img src={slides[selectedImage]} alt={`banner${selectedImage + 1}`} />
             </Link>
         </div>
